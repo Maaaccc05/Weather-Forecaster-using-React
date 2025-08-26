0d9358f0f334786a7e015b952db3227b5ce9c2aa
Mayuresh Kamble <mackam30@gmail.com>
2025-08-26 23:57:57 +0530

    added some functions

---
+++ weather/src/components/WeatherBackground.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Thunderstorm from '../assets/Thunderstorm.gif';
+import Rain from '../assets/Rain.gif';
+import SnowDay from '../assets/Snow.gif';
+import ClearDay from '../assets/ClearDay.gif';
+import ClearNight from '../assets/ClearNight.gif';
+import CloudsDay from '../assets/CloudsDay.gif';
+import CloudsNight from '../assets/CloudsNight.gif';
+import Haze from '../assets/Haze.gif';
+import video from '../assets/video1.mp4';
+
+const WeatherBackground = ({ condition }) => {
+  const gifs = {
+    Thunderstorm,
+    Drizzle: Rain,
+    Rain,
+    Snow: SnowDay,
+    Clear: { day: ClearDay, night: ClearNight },
+    Clouds: { day: CloudsDay, night: CloudsNight },
+    Mist: Haze,
+    Smoke: Haze,
+    Haze,
+    Fog: Haze,
+    default: video
+  };
+}
+const getBackground = () => {
+    if(!condition) return gifs.default;
+    const weatherType = condition.name;
+    const asset = gifs[weatherType]
+
+    if(!asset) return gifs.default
+    if(typeof async === 'object')
+    return condition.isDay ? asset.Day : asset.night
+}
