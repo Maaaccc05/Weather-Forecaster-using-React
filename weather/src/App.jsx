6444bab452bcad822bedaee1457acb6ed48f98b6
Mayuresh Kamble <mackam30@gmail.com>
2025-08-27 23:48:17 +0530

    fetching api keys

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -7,6 +7,12 @@ const App = () => {
   const [city, setCity] = useState('')
   const [suggestion, setSuggestion] = useState([])
 
+  const API_KEY = '501cf176e7f80eac85a43d613b337237'
+
+  // https://api.openweathermap.org/data/2.5/weather?lat=${s.lat}&lon=${s.lon}&appid={API_KEY}&units=metric 
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} city name api key
+
   const getWeatherCondition = () => weather && ({
     main: weather.weather[0].main,
     isDay: Date.now()/1000 > weather.sys.sunrise && Date.now()/1000 < weather.sys.sunset
@@ -25,6 +31,16 @@ const App = () => {
       {weather ? (
         <form onSubmit={handleSearch} className='flex flex-col relative'>
           <input value={city} onChange={(e) => setCity(e.target.value)} placeholder='Enter Your City or Country Name' className='mb-4 p-3 rounded border border-white bg-transparent text-white placeholder-white focus:outline-none focus:border-blue-300 transition duration-300'/>
+
+          {suggestion.length > 0 && (
+            <div className='absolute top-12 left-0 right-0 bg-transparent shadow-md rounded z-10'>
+              {suggestion.map((s) => (
+                <button>
+
+                </button>
+              ))}
+            </div>
+          )}
         </form>
       )}
       </div>
