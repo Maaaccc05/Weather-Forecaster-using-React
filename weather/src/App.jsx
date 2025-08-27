499570cb9f83f1a7e495cea60a6b5695949f286b
Mayuresh Kamble <mackam30@gmail.com>
2025-08-27 15:51:22 +0530

    added sone backgrounds video and gifs

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WeatherBackground from './components/WeatherBackground'
 
 const App = () => {
+
+  const [weather, setWeather] = useState(null)
 
   const getWeatherCondition = () => weather && ({
     main: weather.weather[0].main,
@@ -11,6 +13,14 @@ const App = () => {
   return (
     <div className='min-h-screen'>
     <WeatherBackground condition={getWeatherCondition()}/>
+
+    <div className='flex items-center justify-center p-6 min-h-screen'>
+      <div className='bg-transparent backdrop-filter backdrop-blur-md rounded-xl shadow-2xl p-8 max-w-md text-white w-full border border-white/30 relative z-10'>
+      <h1 className='text-4xl font-extrabold text-center mb-6'>
+        Weather Forecaster
+      </h1>
+      </div>
+    </div>
   </div>
   )
 }
